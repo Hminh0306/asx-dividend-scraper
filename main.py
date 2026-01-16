c1c9f8563b2055014f15a62e6948c5ec844b1e34
ducminhyolo <116327938+aminpepezethun@users.noreply.github.com>
2026-01-16 21:32:06 +0800

    Testing code inside main.py for type and check for async function running from import

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from app.scraper_functions import scraper
 
 # Main function for scraping, write to database and display on Google Sheet
 async def main():
+    # [WARNING]: THIS IS TEST CODE ONLY FOR THE TYPE OF THE OUTPUT
     results = await scraper()
     print(type(results))
     print(results)

cbcd50c65a4eccfb475f7c21eb46287735d7a017
ducminhyolo <116327938+aminpepezethun@users.noreply.github.com>
2026-01-16 20:55:40 +0800

    Adding export_functions.py for export and update functions

---
+++ app/export_functions.py
@@ -0,0 +1,14 @@
+import os
+
+
+# Export functions to Downloads directory
+def export_to_downloads(results):
+    return
+
+# Update results to Google Sheet:
+def update_to_google_sheets(db_query):
+    return
+
+# Save results to database
+def save_to_db(results):
+    return

47c87630e5fdb10b7f33e44c850e7f6ea352e928
ducminhyolo <116327938+aminpepezethun@users.noreply.github.com>
2026-02-01 17:50:28 +0800

    Remove wrong package

--- scraper_playwright.py
+++ scraper_playwright.py
@@ -1,9 +1,7 @@
 import asyncio
 from datetime import datetime
-from pathlib import Path
-from datetime import datetime
 from app.scraper_functions import scraper
-from app.redshift_functions import update_to_redshift, fetch_latest_data_from_redshift_on
+from app.redshift_functions import update_to_redshift
 
 today_str = datetime.now().isoformat()
 

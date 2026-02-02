2825113f3de1a2c8550b57f2257112b2bf544d4e
ducminhyolo <116327938+aminpepezethun@users.noreply.github.com>
2026-02-03 01:17:34 +0800

    Adding functions for AWS S3 Bucket for 'cached' latest fetch in .json format for quick data retrieval

--- app/redshift_functions.py
+++ app/redshift_functions.py
@@ -3,10 +3,14 @@ import redshift_connector
 import pandas as pd
 from datetime import datetime
 from dotenv import load_dotenv
+import boto3
+from botocore.exceptions import NoCredentialsError, ClientError
 
 load_dotenv()
 
-def get_connection():
+# ------------ Redshift Functions --------------------
+
+def get_redshift_connection():
     """
     Connects to Redshift
     """
@@ -24,6 +28,7 @@ def get_connection():
         conn.autocommit = True
         print("✅ Successfully connected to Redshift Database")
         return conn
+    
     except ValueError:
         print(f"⚠️ Invalid REDSHIFT_PORT: '{port_env}'. Defaulting to 5439.")
         # Optional: retry connection with 5439 here or just fail
@@ -32,11 +37,11 @@ def get_connection():
         print(f"❌ Redshift Error: {e}")
         return None
 
-def update_to_redshift(data_results):
+def upload_to_redshift(data_results):
     if not data_results:
         print("⚠️ No data results provided to update_to_redshift.")
         return
-    conn = get_connection()
+    conn = get_redshift_connection()
     if not conn:
         print("❌ Could not establish Redshift connection. Data not saved.")
         return
@@ -80,3 +85,61 @@ def update_to_redshift(data_results):
     finally:
         conn.close() # Close after use - prevent resource leakage
         print("🔌 Redshift connection closed.")
+
+# ------------ S3 Functions --------------------
+
+def get_s3_connection():
+    """
+    Connects to S3 
+    """
+    try:
+        s3_client = boto3.client(
+                's3',
+                aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
+                aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
+                region_name=os.getenv("AWS_REGION")
+            )
+        
+        print("✅ Successfully connected to S3 Client")
+        return s3_client
+
+    except ClientError as e:
+        print("❌ S3 Client failed to connect")
+        return 
+
+
+def upload_to_s3(data_results):
+    if not data_results:
+        print("❌ update_to_s3: No data results found")
+        return
+    
+    # Establishing and checking connection to S3
+    s3_client = get_s3_connection()
+
+    if not s3_client:
+        print("❌ Client failed to conenct. Aborting upload data to S3 bucket...")
+        return
+    
+    try:
+        # upload to S3 bucket  
+        bucket_name = os.getenv('S3_BUCKET_NAME')
+        file_name = "latest_data.json"
+
+        json_data = pd.DataFrame(data_results).to_json(orient="records")
+
+        """
+        Replacing previous data with the same key to new data 
+        - So no appending data but rather overwriting previous JSON to new JSON
+        """
+        s3_client.put_object(
+            Bucket=bucket_name,
+            Key=file_name,
+            Body=json_data,
+            ContentType='applications/json', # read and not download on browser
+            CacheControl='no-cache, no-store, must-revalidate' # ensure no cached old version of the data for the browser
+        )
+
+        print(f"✅ Successfully streamed newest data - {file_name} to S3 bucket {bucket_name}")
+
+    except Exception as e:
+        print(f"❌ S3 Upload Error: {e}")

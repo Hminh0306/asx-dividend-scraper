fa926ee38bde63721426b999b3fe9591d8c72951
ducminhyolo <116327938+aminpepezethun@users.noreply.github.com>
2026-02-04 02:43:55 +0800

    Adding function to download file for backend testing

---
+++ app/export_functions.py
@@ -0,0 +1,19 @@
+import pandas as pd 
+from pathlib import Path
+
+DOWNLOAD_PATH = Path.home() / 'Downloads'
+
+def _get_timestamp(data_results):
+    _timestamp = data_results[0]['Last Updated']
+    return _timestamp
+
+def export_to_downloads(data_results):
+    _timestamp = _get_timestamp(data_results)
+    
+    filename = f"asx-dividend-{_timestamp}.csv"
+    filepath = DOWNLOAD_PATH / filename
+
+    df = pd.DataFrame(data_results)
+    df.to_csv(filepath, index=False)
+
+    print("Downloaded csv file to Download folder")
